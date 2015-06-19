d888aba41ddf3cf9178f5d328f55fa17f08bfa50
Samuel Chun <ephapox@gmail.com>
2015-06-19 16:24:08 -0700

    interests save and can be retrieved via ActiveRecord.

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -10,14 +10,22 @@ class RemindersController < ApplicationController
   def new
   	@reminder = Reminder.new
 		@person = Person.new
+		@reminder_type = ReminderType.new
+		@interest = Interest.new
   end
 
   def create
   	@reminder = Reminder.new(reminder_params)
 		@person = Person.find_or_create_by(name: params[:person][:name])
+		@reminder_type = ReminderType.find_or_create_by(event:params[:reminder_type][:event])
+		@interest = Interest.find_or_create_by(name:params[:interest][:name])
+
 		@person.reminders << @reminder
+		@person.interests << @interest
+		@reminder_type.reminders << @reminder
+
   	@reminder.user_id = current_user.id
-  	if @reminder.save && @person.save
+  	if @reminder.save && @person.save && @reminder_type.save 
   		redirect_to reminders_path
   	else
   		render :new
@@ -31,9 +39,6 @@ class RemindersController < ApplicationController
 private
 	def reminder_params
 		params.require(:reminder).permit(:when, :send_text, :send_email, :person_id)
-	end
-	def person_params
-	  params.require(:person).permit(:name)
 	end
 	def find_reminder
 		@reminder = Reminder.find(params[:id])
--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -1,8 +1,5 @@
 class Reminder < ActiveRecord::Base
   belongs_to :user
   belongs_to :person
-  belongs_to :reminder_type
-
-  accepts_nested_attributes_for :person
-
+  belongs_to :reminder_type  
 end
