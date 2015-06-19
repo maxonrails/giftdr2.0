c8247191e1eab51cab09da2c3930d63e9d68d91a
Samuel Chun <ephapox@gmail.com>
2015-06-18 19:23:21 -0700

    seeded file, all relationships are working!

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(name:"sam",email:"sam@sam.sam")
+Person.create(name:"mark")
+Interest.create(name:"coding")
+ReminderType.create(event:"birthday")
+PersonInterest.create(person_id:1, interest_id:1)
+Reminder.create(user_id:1,reminder_type_id:1,person_id:1)
