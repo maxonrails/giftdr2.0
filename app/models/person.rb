d5ad4a2e32a7d18374464bef14ae4b7d683a10b9
Samuel Chun <ephapox@gmail.com>
2015-06-19 10:10:34 -0700

    if you delete a person, you will delete all reminders for them.

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,5 @@
 class Person < ActiveRecord::Base
 	has_many :person_interests
-	has_many :interests, through: :person_interests 
-	has_many :reminders
-end 
+	has_many :interests, through: :person_interests, dependent: :destroy
+	has_many :reminders, dependent: :destroy
+end
