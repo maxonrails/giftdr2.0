1cb65b3d7fef198a68054e91358ca33f5e438636
Samuel Chun <ephapox@gmail.com>
2015-06-19 11:17:40 -0700

    user data can be edited through user settings page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,33 @@
 class UsersController < ApplicationController
+  before_action :find_user, only:[:show,:edit,:destroy,:update]
+  before_action :logged_in?, only:[:show,:edit,:update,:destroy]
   def index
   end
 
   def show
+
   end
 
   def new
   end
 
   def edit
+
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to reminders_path
+    else
+      render :edit
+    end
+  end
+
+private
+  def user_params
+    params.require(:user).permit(:name,:email,:phone)
+  end
+  def find_user
+    @user = User.find(params[:id])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,11 @@ class User < ActiveRecord::Base
 	has_many :reminders
 	class << self
 		def from_omniauth auth_hash
-			user = find_or_create_by(uid: auth_hash['uid'], provider: auth_hash['provider'])
-			user.name = auth_hash['info']['name']
-			user.image_url = auth_hash['info']['image']
-			user.email = auth_hash['info']['email']
-			user.save!
-			user
+			user = find_or_create_by(uid: auth_hash['uid'], provider: auth_hash['provider']) do |u|
+				u.name = auth_hash['info']['name']
+				u.image_url = auth_hash['info']['image']
+				u.email = auth_hash['info']['email']
+			end
 		end
 	end
 end
