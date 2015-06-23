7ecfd20700d5bca53d680ded5ba118e03f49fd4f
Your Name <nicholasdalke@gmail.com>
2015-06-23 09:51:16 -0700

    tests! Person validation specs

--- spec/features/items_specs_spec.rb
+++ spec/features/items_specs_spec.rb
@@ -1,9 +1,9 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.feature "ItemsSpecs", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
-end    
+# RSpec.feature "ItemsSpecs", type: :feature do
+#   pending "add some scenarios (or delete) #{__FILE__}"
+# end    
 
-	feature "a user can create a new user" do
-		scenario "valid item" do
-			visit '/'
+# 	feature "a user can create a new user" do
+# 		scenario "valid item" do
+# 			visit '/'
