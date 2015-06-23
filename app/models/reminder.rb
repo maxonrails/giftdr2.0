b257f2274360c607622987fce64dec1356f08d15
Your Name <nicholasdalke@gmail.com>
2015-06-23 10:35:22 -0700

    interest model validated with name

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -1,5 +1,6 @@
 class Reminder < ActiveRecord::Base
-	validates :when, presence: true 
+	validates :when, presence: true			
+	
   belongs_to :user
   belongs_to :person
   belongs_to :reminder_type  
--- spec/models/reminder_spec.rb
+++ spec/models/reminder_spec.rb
@@ -2,7 +2,12 @@ require "rails_helper"
 
 RSpec.describe Reminder, type: :model do 
 	it "is invalid without a date" do
-		reminder = Reminder.new(when: nil) 
+		reminder = Reminder.new(:when => nil) 
 		expect(reminder).to be_invalid
 	end 
+
+	# it "is invalid with a date that's passed already" do
+	# 	reminder = Reminder.new(:when => Time.now - 3000)  
+	# 	expect(reminder).to be_invalid 
+	# end 
 end
